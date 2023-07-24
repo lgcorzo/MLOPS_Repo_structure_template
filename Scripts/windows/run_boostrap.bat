d964d2e6f697342b439ed762d184962d8e8b589d
L.corzo <l.corzo@lantek.com>
2023-07-24 23:23:34 +0200

    bootsrap bat example

--- Scripts/windows/run_boostrap.bat
+++ Scripts/windows/run_boostrap.bat
@@ -1,10 +1,9 @@
 set BASE_CONDA="C:\Users\l.corzo\AppData\Local\miniconda3"
-set PYTHONPATH="C:\Users\l.corzo\source\repos\MLOPS_Repo_structure_template"
+set PYTHONPATH="C:\Users\l.corzo\source\repos\MLOPS_Repo_structure_template _test"
 call %BASE_CONDA%\Scripts\activate.bat %BASE_CONDA%
 call conda activate dataingest_localdev_env
 cd %PYTHONPATH%
-call python -m Code.Bootstrap.bootstrap.py  ^
+call python Code/Bootsrap/bootsrap.py  ^
 --directory %PYTHONPATH% ^
 --project_name "Monitoring" ^
---sonar_key "Monitoring" ^
-
+--sonar_key "Monitoring"
