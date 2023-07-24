bdb3c1b3da3d26832c54db8ef96a51ad243c4847
L.corzo <l.corzo@lantek.com>
2023-07-24 23:43:45 +0200

    bootsrap.bat path correction

--- Scripts/windows/run_boostrap.bat
+++ Scripts/windows/run_boostrap.bat
@@ -1,5 +1,5 @@
 set BASE_CONDA="C:\Users\l.corzo\AppData\Local\miniconda3"
-set PYTHONPATH="C:\Users\l.corzo\source\repos\MLOPS_Repo_structure_template _test"
+set PYTHONPATH="C:\Users\l.corzo\source\repos\MLOPS_Repo_structure_template"
 call %BASE_CONDA%\Scripts\activate.bat %BASE_CONDA%
 call conda activate dataingest_localdev_env
 cd %PYTHONPATH%
