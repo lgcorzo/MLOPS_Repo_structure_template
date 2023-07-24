c36d490493b5aecbe1bd88061d6b9b38804bb029
L.corzo <l.corzo@lantek.com>
2023-07-24 21:56:57 +0200

    boostrap run script example

--- Scripts/windows/run_boostrap.bat
+++ Scripts/windows/run_boostrap.bat
@@ -1,10 +1,10 @@
-set root="path the the conda base env"
-set PYTHONPATH="path to the root folder"
-call %root%\Scripts\activate.bat %root%
-call conda activate BU_template_env
+set BASE_CONDA="C:\Users\l.corzo\AppData\Local\miniconda3"
+set PYTHONPATH="C:\Users\l.corzo\source\repos\MLOPS_Repo_structure_template"
+call %BASE_CONDA%\Scripts\activate.bat %BASE_CONDA%
+call conda activate dataingest_localdev_env
 cd %PYTHONPATH%
-call python -m Notebooks.BusinessUnderstanding.bootstrap --tenantid "SSC" ^
---plantid "SSC_Plant" ^
---path_org_data "C:\Users\l.corzo\OneDrive - LANTEK SHEET METAL SOLUTIONS SL\LANTEK\working\Repositories\Data\Raw\Merlin" ^
---path_dst_data "C:\Users\l.corzo\OneDrive - LANTEK SHEET METAL SOLUTIONS SL\LANTEK\working\Repositories\Data\Staging\Merlin\Smartquoting" ^
---org_data_type "parquet"
+call python -m Code.Bootstrap.bootstrap.py  ^
+--directory %PYTHONPATH% ^
+--project_name "Monitoring" ^
+--sonar_key "Monitoring" ^
+
