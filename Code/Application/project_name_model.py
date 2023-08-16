c4bc8e4b56eb0924ce3f9d95ca32fc7794112fa3
L.corzo <l.corzo@lantek.com>
2023-08-16 20:49:52 +0200

    added LLM load function

--- Code/Application/project_name_model.py
+++ Code/Application/project_name_model.py
@@ -1,9 +1,11 @@
-import pandas as pd
-
-from sklearn.base import BaseEstimator
 from typing import Any, List
 
-from Code.Application.project_name_algorithm import jacc_metric_multiset, read_cnc_csv
+import pandas as pd
+from sklearn.base import BaseEstimator
+from transformers import AutoModel, AutoTokenizer
+
+from Code.Application.project_name_algorithm import (jacc_metric_multiset,
+                                                     read_cnc_csv)
 
 NUM_COMP = 10
 names_transform = {
@@ -24,48 +26,33 @@ def get_file_column_from_probea_results(row: pd.DataFrame) -> pd.Series:
 
 class ProjectNameModel(BaseEstimator):
     """Mixin class for ProjectNameModel classifiers in scikit-learn format."""
-    knowledge_kgrams: List[Any]
+    knowledge_tokenized: List[Any]
     knowledge_cnc_name: List[Any]
+    pretrained_model: Any
+    pretrained_tokenizer: Any
     cnc_df: pd.DataFrame
 
     def __init__(self, cnc_path) -> None:
-        self.knowledge_kgrams = []
+        self.knowledge_tokenized = []
         self.knowledge_cnc_name = []
+        self.pretrained_model = None
+        self.pretrained_tokenizer = None
         self.cnc_df = read_cnc_csv(cnc_path)
 
+    def load_pretrained_llm(self, llm_type: str = "microsoft/codebert-base") -> None:
+        if (self.pretrained_model is None):
+            self.pretrained_model = AutoModel.from_pretrained(llm_type)
+        if (self.pretrained_tokenizer is None):
+            self.pretrained_tokenizer = AutoTokenizer.from_pretrained(llm_type)
+
     def fit(self, x_in: [list], y_in: list):
-        """Perform fit on x_in and returns labels for X.
-
-               Parameters
-               ----------
-               x_in : {array-like, sparse matrix} of shape (n_samples, n_features)
-                   The input samples.
-
-               y_in : array with the machine labels
-
-               Returns
-               -------
-               self : ndarray of shape (n_samples,)
-
-        """
-        self.knowledge_kgrams = x_in
+        self.knowledge_tokenized = x_in
         self.knowledge_cnc_name = y_in
         return self
 
     def predict_probea(self, x_in: pd.Series, num_results: int = NUM_COMP) -> pd.DataFrame:
-        """Perform prediction on x_in and returns labels for x_in returning metric, post file and machine
-
-               Parameters
-               ----------
-               x_in : List of lists  of shape (n_samples, n_features)
-                   The input samples.
-               num_results : int Number of elements to show in the list of  similar results
-               Returns
-               -------
-               self : dataframe of shape with the list of metrics
-        """
         df_in = pd.DataFrame()
-        df_in['cnc_db'] = self.knowledge_kgrams
+        df_in['cnc_db'] = self.knowledge_tokenized
         df_in['f_cluster'] = self.knowledge_cnc_name
         cnc_df = self.cnc_df.copy()
         cnc_df['f_cluster'] = cnc_df['CNC'] + cnc_df['Extension'].fillna('')
@@ -86,19 +73,6 @@ class ProjectNameModel(BaseEstimator):
 
     # Revisar loop duplicado
     def score(self, x_test: list, y_test: list) -> float:
-        """
-        Return the percentage of the ratio between the matches and the total compared
-
-        Parameters:
-        -----------
-        x_test : list of the kgrams from the test cnc files
-        y_test : list of the cnc names from the test cnc files
-
-        Returns
-        -------
-        score : float
-            prevalence = positive/(positive+negative)
-        """
         p = 0
         n = 0
         data = {'cnc_db': x_test, 'f_cluster': y_test}
@@ -117,18 +91,6 @@ class ProjectNameModel(BaseEstimator):
         return p / (p + n)
 
     def predict(self, x_in: list) -> pd.Series:
-        """Perform prediction on x_in and returns labels for x_in.
-
-               Parameters
-               ----------
-               x_in : {array-like, dict}
-                   The input samples.
-
-               Returns
-               -------
-               result_out : result cluster series
-        """
-
         data = {'cnc_db': x_in}
         data_df = pd.DataFrame(data)
         result_df = data_df.apply(self.predict_probea, num_results=1, axis=1)
