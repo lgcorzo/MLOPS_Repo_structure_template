a2b180cc5c1195b645339d1e6a364e059a24fcc2
L.corzo <l.corzo@lantek.com>
2023-08-18 09:09:59 +0200

    added unit test model

--- Tests/Unit/Code/Application/test_project_name_model.py
+++ Tests/Unit/Code/Application/test_project_name_model.py
@@ -3,6 +3,7 @@ from unittest.mock import MagicMock
 import os
 import torch
 import pytest
+import pandas as pd
 from Code.Application.project_name_model import (ProjectNameModel, cosine_similarity)
 
 
@@ -13,16 +14,52 @@ CNC_PATH = os.path.join(cwd, 'Fixtures', 'CNC', CSV_FILE)
 MODEL_NAME = "microsoft/codebert-base"
 
 
+@pytest.fixture
 @mock.patch("Code.Application.project_name_model.read_cnc_csv")
-def test_init_project_name_model(mock_read_csv: mock) -> None:
-    mock_read_csv.return_value = "test"
+def init_model_class_read_mocked(mock_read: mock):
+    data = {
+        'WrkRef': ['Machine', 'Machine', 'Machine', 'Machine'],
+        'CNC': [1913, 1914, 1915, 1916],
+        'DIS_PsfFile': ['pst.exe', 'pst.exe', 'pst.exe', 'pst.exe'],
+        'DIS_CfgFile': ['pst.se1', 'pst.se1', 'pst.se1', 'pst.se1'],
+        'PrdRefDst': ['Pieza_prueba_Maxwell', 'Pieza_prueba_Maxwell_2', 'DXFPART7', 'DXFPAR0105'],
+        'Extension': ['.CNC', '.CNC', '.CNC', '.CNC']
+    }
+
+    cnc_df = pd.DataFrame(data)
+    mock_read.return_value = cnc_df
     model = ProjectNameModel(CNC_PATH)
-    assert model.cnc_df == "test"
+    return model
+
+
+@pytest.fixture
+def vectors():
+    x = torch.tensor([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+    y = torch.tensor([[2.0, 3.0, 4.0], [5.0, 6.0, 7.0]])
+    return x, y
+
+
+@mock.patch("Code.Application.project_name_model.read_cnc_csv")
+def test_init_project_name_model(mock_read: mock) -> None:
+
+    data = {
+        'WrkRef': ['Machine', 'Machine', 'Machine', 'Machine'],
+        'CNC': [1913, 1914, 1915, 1916],
+        'DIS_PsfFile': ['pst.exe', 'pst.exe', 'pst.exe', 'pst.exe'],
+        'DIS_CfgFile': ['pst.se1', 'pst.se1', 'pst.se1', 'pst.se1'],
+        'PrdRefDst': ['Pieza_prueba_Maxwell', 'Pieza_prueba_Maxwell_2', 'DXFPART7', 'DXFPAR0105'],
+        'Extension': ['.CNC', '.CNC', '.CNC', '.CNC']
+    }
+
+    expected_df = pd.DataFrame(data)
+    mock_read.return_value = expected_df
+    model = ProjectNameModel(CNC_PATH)
+    mock_read.assert_called_with(CNC_PATH)
     assert model.knowledge_tokenized == []
     assert model.knowledge_cnc_name == []
     assert model.pretrained_model is None
     assert model.pretrained_tokenizer is None
-    mock_read_csv.assert_called_once()
+    pd.testing.assert_frame_equal(expected_df, model.cnc_df)
 
 
 @mock.patch("Code.Application.project_name_model.AutoModel")
@@ -30,10 +67,11 @@ def test_init_project_name_model(mock_read_csv: mock) -> None:
 @mock.patch("Code.Application.project_name_model.logging")
 def test_load_pretrained_llm_model_none_tokenizer_none(mock_logging: mock,
                                                        mock_autotockenizer: mock,
-                                                       mock_automodel: mock) -> None:
+                                                       mock_automodel: mock,
+                                                       init_model_class_read_mocked) -> None:
     mock_autotockenizer.from_pretrained.return_value = "tockenizer"
     mock_automodel.from_pretrained.return_value = "model"
-    model = ProjectNameModel(CNC_PATH)
+    model = init_model_class_read_mocked
     model.load_pretrained_llm(MODEL_NAME)
     mock_autotockenizer.from_pretrained.assert_called_with(MODEL_NAME)
     mock_automodel.from_pretrained.assert_called_with(MODEL_NAME)
@@ -47,10 +85,11 @@ def test_load_pretrained_llm_model_none_tokenizer_none(mock_logging: mock,
 @mock.patch("Code.Application.project_name_model.logging")
 def test_load_pretrained_llm_tokenizer_none(mock_logging: mock,
                                             mock_autotockenizer: mock,
-                                            mock_automodel: mock) -> None:
+                                            mock_automodel: mock,
+                                            init_model_class_read_mocked) -> None:
     mock_autotockenizer.from_pretrained.return_value = "tockenizer"
     mock_automodel.from_pretrained.return_value = "model"
-    model = ProjectNameModel(CNC_PATH)
+    model = init_model_class_read_mocked
     model.pretrained_model = "model"
     model.pretrained_tokenizer = None
     model.load_pretrained_llm(MODEL_NAME)
@@ -66,10 +105,10 @@ def test_load_pretrained_llm_tokenizer_none(mock_logging: mock,
 @mock.patch("Code.Application.project_name_model.logging")
 def test_load_pretrained_llm_model_none(mock_logging: mock,
                                         mock_autotockenizer: mock,
-                                        mock_automodel: mock) -> None:
+                                        mock_automodel: mock, init_model_class_read_mocked) -> None:
     mock_autotockenizer.from_pretrained.return_value = "tockenizer"
     mock_automodel.from_pretrained.return_value = "model"
-    model = ProjectNameModel(CNC_PATH)
+    model = init_model_class_read_mocked
     model.pretrained_model = None
     model.pretrained_tokenizer = "tockenizer"
     model.load_pretrained_llm(MODEL_NAME)
@@ -78,13 +117,6 @@ def test_load_pretrained_llm_model_none(mock_logging: mock,
     mock_logging.info.assert_called_with(f'{MODEL_NAME} models loaded')
     assert model.pretrained_model == "model"
     assert model.pretrained_tokenizer == "tockenizer"
-
-
-@pytest.fixture
-def vectors():
-    x = torch.tensor([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
-    y = torch.tensor([[2.0, 3.0, 4.0], [5.0, 6.0, 7.0]])
-    return x, y
 
 
 @mock.patch("Code.Application.project_name_model.logging")
@@ -99,10 +131,10 @@ def test_cosine_similarity(mock_logging: mock, vectors):
 @mock.patch("Code.Application.project_name_model.logging")
 @mock.patch("Code.Application.project_name_model.cosine_similarity")
 def test_compare_documents(mock_cosine_similarity: mock,
-                           mock_logging: mock) -> None:
+                           mock_logging: mock, init_model_class_read_mocked) -> None:
     doc1 = 'ABC'
     doc2 = 'CDE'
-    model = ProjectNameModel(CNC_PATH)
+    model = init_model_class_read_mocked
     model.pretrained_tokenizer = MagicMock()
     model.pretrained_tokenizer().input_ids = 'test'
     model.pretrained_model = MagicMock()
