35d415b5165dc44ee1d204a19ae555198299da4d
L.corzo <l.corzo@lantek.com>
2023-09-14 23:59:39 +0200

    mlflow sh

--- be_entrypoint.sh
+++ be_entrypoint.sh
@@ -8,7 +8,7 @@ conda init
 conda activate code_development_env
 
 echo "Starting mlflow..."
-mlflow server --backend-store-uri sqlite:///mlflow.db --default-artifact-root ./artifacts --host '127.0.0.1' -p 1234 &
+mlflow server --backend-store-uri sqlite:///artifacts/mlflow.db --default-artifact-root ./artifacts --host $BE_HOST -p 1234 &
 
 echo "Starting backend..."
 gunicorn Code.Controller.app:app --workers 4 --threads 2 --bind $BE_HOST:$BE_PORT
